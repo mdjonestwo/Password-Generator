efeba278207c4be38c3b949304879adb09ec9c99
Sage <mdjones2@protonmail.com>
2020-10-09 22:04:22 -0400

    web app finalized

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Arrays to randomize 
-let specialChar = [
+// Arrays to randomiz
+var specialChar = [
   "!",
   "@",
   "#",
-  "4",
+  "$",
   "%",
   "^",
   "&",
@@ -20,8 +20,7 @@ let specialChar = [
   "~",
   "?",
 ];
-
-let lowerCase = [
+var lowerCase = [
   "a",
   "b",
   "c",
@@ -49,8 +48,7 @@ let lowerCase = [
   "y",
   "z",
 ];
-
-let upperCase = [
+var upperCase = [
   "A",
   "B",
   "C",
@@ -78,37 +76,88 @@ let upperCase = [
   "Y",
   "Z",
 ];
-
-let number = [
-  "1", 
-  "2",
-  "3", 
-  "4",
-  "5",
-  "6", 
-  "7", 
-  "8", 
-  "9",
-  "0"
-];
-
-// Questions for user
-let lengthOfPass = prompt('How many characters would you like in your password? (Enter a number between 8 and 128)')
-
-let ifSpecialChar = confirm("Would you like special characters in your password?");
-let ifLowerCase = confirm("Would you like lower case characters in your password?");
-let ifUpperCase = confirm("Would you like upper case characters in your password?")
-let ifNumber = confirm("Would you like numbers in your password?");
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 
-randomFunc = {
-  Loe
+//takes user input for password options and turns result into an object
+function setPassOptions() {
+  var length = parseInt(
+    prompt(
+      "How many characters would you like in your password? (Enter a number between 8 and 128)"
+    )
+  );
+  if (isNaN(length)) {
+    alert("Must provide a number!");
+    return;
+  }
+  if (length < 8 || length > 128) {
+    alert("Must be a number between 8 and 128!");
+    return;
+  }
+
+  var ifSpecialChar = confirm(
+    "Would you like special characters in your password?"
+  );
+
+  var ifLowerCase = confirm(
+    "Would you like lower case characters in your password?"
+  );
+
+  var ifUpperCase = confirm(
+    "Would you like upper case characters in your password?"
+  );
+
+  var ifNumber = confirm("Would you like numbers in your password?");
+  if (!ifSpecialChar && !ifLowerCase && !ifUpperCase && !ifNumber) {
+    alert("Must select at least one character type!");
+    return;
+  }
+
+  // Password Conditions Object & Properties
+  var passConditionsObj = {
+    length: length,
+    ifSpecialChar: ifSpecialChar,
+    ifLowerCase: ifLowerCase,
+    ifUpperCase: ifUpperCase,
+    ifNumber: ifNumber,
+  };
+  return passConditionsObj;
 }
 
+//randomizes data in an array 
+function randomizer(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randomIndex];
+  return randomElement;
+}
 
-
-generatePassword() {
-
+function generatePassword() {
+  // turns object into variable
+  var options = setPassOptions();
+  // array for new password
+  var resolve = [];
+  //if objects(passConditionsObj are true add to possibleChar array. else, dont.)
+  var possibleChar = [];
+  if (options.ifSpecialChar) {
+    possibleChar = possibleChar.concat(specialChar);
+  }
+  if (options.ifLowerCase) {
+    possibleChar = possibleChar.concat(lowerCase);
+  }
+  if (options.ifUpperCase) {
+    possibleChar = possibleChar.concat(upperCase);
+  }
+  if (options.ifNumber) {
+    possibleChar = possibleChar.concat(number);
+  }
+ // console.log(possibleChar);
+//randomize the possibleChar array and push it to the resolve array to create the new password by the length added by user
+  for (var i = 0; i < options.length; i++) {
+    var possiblePass = randomizer(possibleChar);
+    resolve.push(possiblePass);
+  }
+  //turns array into string
+  return resolve.toString();
 }
 
 // Write password to the #password input
@@ -117,7 +166,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
